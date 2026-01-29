23c9a4e755c3b1bd903b9473fb9d353f59167dbb
rvf1-k <125549569+rvf1-k@users.noreply.github.com>
2026-01-29 16:16:27 +0100

    test

--- shortcut_aules.js
+++ shortcut_aules.js
@@ -1,15 +1,12 @@
 // ==UserScript==
-// @name         Enviar con Ctrl+Enter en Aules GVA
+// @name         Manabase Tool Auto-Analyzer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Enviar tareas usando con Ctrl + Enter en Aules de la Generalitat Valenciana
-// @author       rvf1-k
-// @match        https://aules.edu.gva.es/fp/mod/assign/view.php*
-// @require      https://code.jquery.com/jquery-3.7.1.min.js
-// @icon         https://portal.edu.gva.es/aules/wp-content/uploads/sites/644/2024/07/cropped-Imagotipo_Aules-32x32.png
-// @source       https://github.com/rvf1-k/Enviar-con-Ctrl-Enter-en-Aules-GVA
-// @updateURL    https://github.com/rvf1-k/Enviar-con-Ctrl-Enter-en-Aules-GVA/blob/main/shortcut_aules.js
-// @downloadURL  https://github.com/rvf1-k/Enviar-con-Ctrl-Enter-en-Aules-GVA/blob/main/shortcut_aules.js
+// @version      3.3.1
+// @description  Auto-triggers analyzers, maintains history, and optimizes basic land distribution
+// @author       pakoito
+// @match        https://ianrh125.github.io/snail-analyzer/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.io
+// @require      https://gist.githubusercontent.com/pakoito/5c7f9b8c35efee0126b2b874beb365db/raw/manabase-optimizer-bundle.js?v=1769692141657
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
