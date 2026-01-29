e5d2ab51d33d5697c70c3f358533652068feff24
rvf1-k <125549569+rvf1-k@users.noreply.github.com>
2026-01-29 19:43:49 +0100

    Fix formatting of run-at directive in user script

--- shortcut_aules.user.js
+++ shortcut_aules.user.js
@@ -11,8 +11,9 @@
 // @updateURL    https://github.com/rvf1-k/Enviar-con-Ctrl-Enter-en-Aules-GVA/raw/refs/heads/main/shortcut_aules.user.js
 // @downloadURL  https://github.com/rvf1-k/Enviar-con-Ctrl-Enter-en-Aules-GVA/raw/refs/heads/main/shortcut_aules.user.js
 // @grant        none
-// @run-at document-end
+// @run-at       document-end
 // ==/UserScript==
+
 (function () {
     'use strict';
 
